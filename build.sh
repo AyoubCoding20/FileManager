402a513189e5a851e9dd72e70193b81f511deaa3
Ayoub Coding <116565401+AyoubCoding20@users.noreply.github.com>
2022-10-27 12:26:05 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+sudo apt update && sudo apt install dotnet-sdk-6.0 powershell powershell-lts code-oss 
+dotnet run
+# You gotta execute dotnet run in the Directory FileManager and dotnet run as root or normal user.
